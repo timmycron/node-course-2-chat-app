6439eb5b83e7ea3ba5b125123342068804c72cd1
timmycron <timmycron@hotmail.com>
2016-12-28 10:57:57 -0800

    Fixed port

--- server/server.js
+++ server/server.js
@@ -3,13 +3,11 @@ const path = require('path');
 
 const publicPath = path.join(__dirname, '..', 'public');
 
-console.log(publicPath);
-
 const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
