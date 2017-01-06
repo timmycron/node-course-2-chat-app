5e8f672f3194a6acdcd50a25ce5a227e616a6daf
timmycron <timmycron@hotmail.com>
2017-01-05 20:47:21 -0800

    added mustache js

--- public/js/index.js
+++ public/js/index.js
@@ -15,23 +15,39 @@ socket.on('disconnect', function() {
 // custom event
 socket.on('newMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = $('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  $('#messages').append(li);
+  var template = $('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  $('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
-  var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = $('<li></li>');
-  var a = $('<a target="_blank">My current location</a>');
 
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = $('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+
+  $('#messages').append(html);
+
+  // var formattedTime = moment(message.createdAt).format('h:mm a');
+  // var li = $('<li></li>');
+  // var a = $('<a target="_blank">My current location</a>');
+  //
   // using functions like text and attr
   // rather than just injecting with template string... if you did that people
   // could inject malicious code
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  $('#messages').append(li);
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+  // $('#messages').append(li);
 });
 
 $('#message-form').on('submit', function(e) {
